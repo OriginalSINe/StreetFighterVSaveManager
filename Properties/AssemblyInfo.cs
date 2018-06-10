d1468f0d6e33f973897fc3764dab964e6993ebf1
Walter Blacke <the.originalsine@gmail.com>
2018-06-10 19:42:10 -0400

    copyright

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StreetFighterVSaveManager")]
-[assembly: AssemblyCopyright("Copyright OriginalSINe © 2016")]
+[assembly: AssemblyCopyright("Copyright OriginalSINe © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
